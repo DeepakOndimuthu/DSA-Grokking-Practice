6a1562d8f70dfd12dac867c8f64b3ddf80bae3c6
deepak <dlipkumar77@gmail.com>
2026-01-10 20:38:51 +0530

    feat: add array interview trap (index vs value confusion))

---
+++ src/Arrays/InterviewTraps/IndexValueTrap.java
@@ -0,0 +1,23 @@
+package Arrays.InterviewTraps;
+
+/*
+ No. 7
+ Problem: Index vs Value Confusion (Trap Question)
+ Pattern: Logical understanding
+ Status: ✅ Solved
+
+ Input: nums = [10, 20, 30]
+ Task: Print index and value correctly
+*/
+
+public class IndexValueTrap {
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("Index: " + i + " Value: " + nums[i]);
+        }
+    }
+}
+
